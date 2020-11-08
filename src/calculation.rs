7d16ca13241bf6ddbce6004b8ed17007283ecc85
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2020-11-08 15:57:58 +0300

    Calculation on features

--- src/calculation.rs
+++ src/calculation.rs
@@ -11,10 +11,48 @@ pub enum Error {
 }
 
 pub fn calculate(a: bool, b: bool, c: bool, d: f64, e: i64, f: i64) -> Result<(HValue, f64), Error> {
-    Ok((HValue::M, 1.0))
+    let h = _h_calc(a, b, c)?;
+    let k = _k_calc(&h, d, e, f);
+    Ok((h, k))
+}
+
+fn _h_calc(a: bool, b: bool, c: bool) -> Result<HValue, Error> {
+    let h = match (a, b, c) {
+        (true, true, false) => Ok(HValue::M),
+        (true, true, true) => Ok(HValue::P),
+        (false, true, true) => Ok(HValue::T),
+        _ => Err(Error::NoMatchingRules)
+    };
+    #[cfg(feature="custom2")]
+    let h = match (a, b, c, h) {
+        (true, true, false, _) => Ok(HValue::T),
+        (true, false, true, _) => Ok(HValue::M),
+        (_, _, _, e) => e
+    };
+    h
+}
+
+fn _k_calc(h: &HValue, d: f64, e: i64, f: i64) -> f64 {
+    let k = match h {
+        &HValue::M => d + (d*e as f64 / 10.0),
+        &HValue::P => d + (d*(e-f) as f64/25.5),
+        &HValue::T => d - (d*f as f64 / 30.0)
+    };
+    #[cfg(feature="custom1")]
+    let k = match (h, k) {
+        (&HValue::P, _) => 2.0*d + (d*e as f64/100.0),
+        (_, k) => k
+    };
+    #[cfg(feature="custom2")]
+    let k = match (h, k) {
+        (&HValue::M, _) => f as f64 + d + (d*e  as f64 / 100.0),
+        (_, k) => k
+    };
+    k
 }
 
 #[test]
+#[cfg(not(feature="custom1"))]
 fn base_test_1() {
     let a = true;
     let b = true;
@@ -28,6 +66,7 @@ fn base_test_1() {
 }
 
 #[test]
+#[cfg(not(feature="custom2"))]
 fn base_test_2() {
     let a = true;
     let b = true;
@@ -37,7 +76,7 @@ fn base_test_2() {
     let f = 7;
     let (h, k) = calculate(a, b, c, d, e, f).unwrap();
     assert_eq!(h, HValue::M);
-    assert_eq!(k, d + (d*e as f64 / 10 as f64));
+    assert_eq!(k, d + (d*e as f64 / 10.0));
 }
 
 #[test]
@@ -50,7 +89,7 @@ fn base_test_3() {
     let f = 7;
     let (h, k) = calculate(a, b, c, d, e, f).unwrap();
     assert_eq!(h, HValue::T);
-    assert_eq!(k, d - (d*f as f64 / 30 as f64));
+    assert_eq!(k, d - (d*f as f64 / 30.0));
 }
 
 
@@ -64,8 +103,8 @@ fn custom1_test() {
     let e = 6;
     let f = 7;
     let (h, k) = calculate(a, b, c, d, e, f).unwrap();
-    assert_eq!(h, HValue::T);
-    assert_eq!(k, 2*d + (d*e/100));
+    assert_eq!(h, HValue::P);
+    assert_eq!(k, 2.0*d + (d*e as f64/100.0));
 }
 
 #[test]
@@ -79,7 +118,7 @@ fn custom2_test_1() {
     let f = 7;
     let (h, k) = calculate(a, b, c, d, e, f).unwrap();
     assert_eq!(h, HValue::T);
-    assert_eq!(k, d - (d*f as f64 / 30 as f64));
+    assert_eq!(k, d - (d*f as f64 / 30.0));
 }
 
 #[test]
@@ -93,7 +132,7 @@ fn custom2_test_2() {
     let f = 7;
     let (h, k) = calculate(a, b, c, d, e, f).unwrap();
     assert_eq!(h, HValue::M);
-    assert_eq!(k, f + d + (d*e / 100));
+    assert_eq!(k, f as f64 + d + (d*e as f64 / 100.0));
 }
 
 #[test]
@@ -107,5 +146,5 @@ fn custom2_test_3() {
     let f = 7;
     let (h, k) = calculate(a, b, c, d, e, f).unwrap();
     assert_eq!(h, HValue::T);
-    assert_eq!(k, d - (d*f as f64 / 30 as f64));
+    assert_eq!(k, d - (d*f as f64 / 30.0));
 }
